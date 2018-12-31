e15ef6628f7c918575142a3084bbe6605f37473e
ellymaimon <eliranmaimon@gmail.com>
2018-12-31 14:39:56 -0500

    Add forgotten data constant to main update function

--- update.js
+++ update.js
@@ -2,6 +2,7 @@ import * as dynamoDbLib from './libs/dynamodb-lib';
 import { success, failure } from './libs/response-lib';
 
 export async function main(event, context) {
+  const data = JSON.parse(event.body);
   const params = {
     TableName: 'notes',
     // 'Key' defines the partition key and sort key of the item to be updated
