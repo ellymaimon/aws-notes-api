73b2f426a389a95aa908b0492ea60e7d0e839707
ellymaimon <eliranmaimon@gmail.com>
2018-12-31 13:52:03 -0500

    Add AWS region config line to the dynamo db lib function call

--- libs/dynamodb-lib.js
+++ libs/dynamodb-lib.js
@@ -1,6 +1,7 @@
-import AWS from "aws-sdk";
+import AWS from 'aws-sdk';
 
 export function call(action, params) {
+  AWS.config.update({ region: 'us-west-2' });
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
   return dynamoDb[action](params).promise();
