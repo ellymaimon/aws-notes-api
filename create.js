3bae45b6e52e260be11c82807f82b399efd5649b
ellymaimon <eliranmaimon@gmail.com>
2018-12-31 13:52:32 -0500

    Remove AWS region config and add white spaces for formatting

--- create.js
+++ create.js
@@ -2,8 +2,6 @@ import uuid from 'uuid';
 import * as dynamoDbLib from './libs/dynamodb-lib';
 import { success, failure } from './libs/response-lib';
 
-AWS.config.update({region: 'us-west-2'});
-
 export async function main(event, context) {
   const data = JSON.parse(event.body);
   const params = {
@@ -13,14 +11,14 @@ export async function main(event, context) {
       noteId: uuid.v1(),
       content: data.content,
       attachment: data.attachment,
-      createdAt: Date.now()      
+      createdAt: Date.now()
     }
   };
 
   try {
     await dynamoDbLib.call('put', params);
     return success(params.Item);
-  } catch(e) {
+  } catch (e) {
     return failure({ status: false });
   }
 }
